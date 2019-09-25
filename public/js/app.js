0969792d2e9c318e80ac0ef88af8f72865741586
Jorge Torreblanca <torrenegra.inv@gmail.com>
2019-09-25 18:29:10 -0500

    Setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -21,7 +21,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    let url = 'http://localhost:3000/weather?address='+location
+    let url = '/weather?address='+location
 
     fetch(url).then((response) => {
     response.json().then((data) => {
